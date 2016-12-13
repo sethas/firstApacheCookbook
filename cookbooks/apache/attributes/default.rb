a182ae8b90c27504c30ff0c2e384021a8d7a76dd
root <root@amitseth74.mylabserver.com>
2016-12-13 05:41:50 +0000

    Commit for 1st time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["amitseth72"] = { "site_title" => "Amit2 website", "port" => 80, "domain" => "amitseth72.mylabserver.com" }
+default["apache"]["sites"]["amitseth72b"] = { "site_title" => "Amit 2b website", "port" => 80, "domain" => "amitseth72b.mylabserver.com" }
+default["apache"]["sites"]["amitseth73"] = { "site_title" => "Amit3 website", "port" => 80, "domain" => "amitseth73.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "debian"
+	default["apache"]["package"] = "apache2"
+end
